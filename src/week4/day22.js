ca98d6fb6bef68105fb2dd515a0549cd02313dfe
Francois Perron <fperron@gmail.com>
2021-12-22 21:07:31 -0500

    Day 22

--- src/week4/day22.js
+++ src/week4/day22.js
@@ -1,40 +1,77 @@
+// part 2
+
+export const executeFullRebootSteps = input => parseRebootSteps(input)
+  .reduce((intersections, step) => findIntersections(intersections, step), [])
+  .map(cubeValue).reduce(sum)
+
+const findIntersections = (intersections, step) => {
+  const newIntersections = intersections
+    .map(intersection => intersect(intersection, step))
+    .filter(newIntersection => newIntersection !== null)
+
+  if (step.on) {
+    newIntersections.push(step)
+  }
+  return [...intersections, ...newIntersections]
+}
+
+
+export const intersect = (cube1, cube2) => {
+  const intersection = {
+    on: !cube1.on,
+    x: { min: Math.max(cube1.x.min, cube2.x.min), max: Math.min(cube1.x.max, cube2.x.max) },
+    y: { min: Math.max(cube1.y.min, cube2.y.min), max: Math.min(cube1.y.max, cube2.y.max) },
+    z: { min: Math.max(cube1.z.min, cube2.z.min), max: Math.min(cube1.z.max, cube2.z.max) }
+  }
+  return intersection.x.min > intersection.x.max || intersection.y.min > intersection.y.max || intersection.z.min > intersection.z.max
+    ? null
+    : intersection
+}
+
+export const cubeValue = cube => (cube.on ? 1 : -1) *
+  (cube.x.max + 1 - cube.x.min) *
+  (cube.y.max + 1 - cube.y.min) *
+  (cube.z.max + 1 - cube.z.min)
+
+// part 1
 export const executeRebootSteps = input => {
 
   let steps = parseRebootSteps(input)
   steps = removeStepsOutside(steps)
 
   const cubes = steps.reduce((cubes, step) => {
-    range(step.minX, step.maxX).map(x => {
-      range(step.minY, step.maxY).map(y => {
-        range(step.minZ, step.maxZ).map(z => {
-          cubes[hash(x, y, z)] = step.state
+    range(step.x.min, step.x.max).map(x => {
+      range(step.y.min, step.y.max).map(y => {
+        range(step.z.min, step.z.max).map(z => {
+          cubes[hash(x, y, z)] = step.on
         })
       })
     })
     return cubes
   }, {})
 
-  return Object.values(cubes).filter(cube => cube === 1).length
+  return Object.values(cubes).filter(on => on === true).length
 }
 
 export const removeStepsOutside = steps => steps
   .filter(step =>
-    step.minX >= -50 && step.maxX <= 50 &&
-    step.minY >= -50 && step.maxY <= 50 &&
-    step.minZ >= -50 && step.maxZ <= 50)
+    step.x.min >= -50 && step.x.max <= 50 &&
+    step.y.min >= -50 && step.y.max <= 50 &&
+    step.z.min >= -50 && step.z.max <= 50)
 
 export const parseRebootSteps = input => input.map(parseRebootStep)
 
 const parseRebootStep = line => {
-  const regex = /(?<state>[a-zA-Z]+) x=(?<minX>-?\d+)..(?<maxX>-?\d+),y=(?<minY>-?\d+)..(?<maxY>-?\d+),z=(?<minZ>-?\d+)..(?<maxZ>-?\d+)/
+  const regex = /(?<on>[a-zA-Z]+) x=(?<minX>-?\d+)..(?<maxX>-?\d+),y=(?<minY>-?\d+)..(?<maxY>-?\d+),z=(?<minZ>-?\d+)..(?<maxZ>-?\d+)/
   const groups = line.match(regex).groups
   return {
-    state: groups['state'] === 'on' ? 1 : 0,
-    minX: parseInt(groups['minX']), maxX: parseInt(groups['maxX']),
-    minY: parseInt(groups['minY']), maxY: parseInt(groups['maxY']),
-    minZ: parseInt(groups['minZ']), maxZ: parseInt(groups['maxZ'])
+    on: groups['on'] === 'on',
+    x: { min: parseInt(groups['minX']), max: parseInt(groups['maxX']) },
+    y: { min: parseInt(groups['minY']), max: parseInt(groups['maxY']) },
+    z: { min: parseInt(groups['minZ']), max: parseInt(groups['maxZ']) }
   }
 }
 
 const range = (start, end) => Array(end - start + 1).fill(0).map((_, idx) => start + idx)
 const hash = (x, y, z) => x + ',' + y + ',' + z
+const sum = (sum, on) => sum + on
--- src/week4/day22.spec.js
+++ src/week4/day22.spec.js
@@ -1,5 +1,5 @@
 import { dailyInputLines } from '../dailyInput.js'
-import { executeRebootSteps, parseRebootSteps, removeStepsOutside } from './day22.js'
+import { executeFullRebootSteps, executeRebootSteps, intersect, parseRebootSteps, removeStepsOutside, cubeValue } from './day22.js'
 
 describe('Day 22: Reactor Reboot', () => {
   const example = ['on x=-20..26,y=-36..17,z=-47..7', 'on x=-20..33,y=-21..23,z=-26..28', 'on x=-22..28,y=-29..23,z=-38..16', 'on x=-46..7,y=-6..46,z=-50..-1', 'on x=-49..1,y=-3..46,z=-24..28', 'on x=2..47,y=-22..22,z=-23..27', 'on x=-27..23,y=-28..26,z=-21..29', 'on x=-39..5,y=-6..47,z=-3..44', 'on x=-30..21,y=-8..43,z=-13..34', 'on x=-22..26,y=-27..20,z=-29..19', 'off x=-48..-32,y=26..41,z=-47..-37', 'on x=-12..35,y=6..50,z=-50..-2', 'off x=-48..-32,y=-32..-16,z=-15..-5', 'on x=-18..26,y=-33..15,z=-7..46', 'off x=-40..-22,y=-38..-28,z=23..41', 'on x=-16..35,y=-41..10,z=-47..6', 'off x=-32..-23,y=11..30,z=-14..3', 'on x=-49..-5,y=-3..45,z=-29..18', 'off x=18..30,y=-20..-8,z=-3..13', 'on x=-41..9,y=-7..43,z=-33..15', 'on x=-54112..-39298,y=-85059..-49293,z=-27449..7877', 'on x=967..23432,y=45373..81175,z=27513..53682']
@@ -13,7 +13,7 @@ describe('Day 22: Reactor Reboot', () => {
       const steps = parseRebootSteps(example)
 
       expect(steps.length).to.equal(22)
-      expect(steps[0]).to.deep.equal({ state: 1, minX: -20, maxX: 26, minY: -36, maxY: 17, minZ: -47, maxZ: 7 })
+      expect(steps[0]).to.deep.equal({ on: true, x: { min: -20, max: 26 }, y: { min: -36, max: 17 }, z: { min: -47, max: 7 } })
     })
 
     it('removes steps outside -50,50', () => {
@@ -28,6 +28,39 @@ describe('Day 22: Reactor Reboot', () => {
 
     it('solve it', () => {
       expect(executeRebootSteps(input)).to.equal(542711)
+    })
+  })
+
+  describe('Part 2: how many cubes are on', () => {
+    it('intersects 2 cubes', () => {
+      const cube1 = { on: true, x: { min: -20, max: 20 }, y: { min: -20, max: 20 }, z: { min: -20, max: 20 } }
+      const cube2 = { x: { min: -10, max: 30 }, y: { min: -10, max: 30 }, z: { min: -10, max: 30 } }
+
+      const intersection = { on: false, x: { min: -10, max: 20 }, y: { min: -10, max: 20 }, z: { min: -10, max: 20 } }
+      expect(intersect(cube1, cube2)).to.deep.equal(intersection)
+    })
+
+    it('intersects 2 cubes is null when no interception', () => {
+      const cube1 = { x: { min: -20, max: 20 }, y: { min: -20, max: 20 }, z: { min: -20, max: 20 } }
+      const cube2 = { x: { min: 21, max: 30 }, y: { min: 21, max: 30 }, z: { min: 21, max: 30 } }
+
+      expect(intersect(cube1, cube2)).to.equal(null)
+    })
+
+    it('calculates the cube value', () => {
+      const cubeOn = { on: true, x: { min: -20, max: 20 }, y: { min: -20, max: 20 }, z: { min: -20, max: 20 } }
+      expect(cubeValue(cubeOn)).to.equal(41 * 41 * 41)
+
+      const cubeOff = { on: false, x: { min: -20, max: 20 }, y: { min: -20, max: 20 }, z: { min: -20, max: 20 } }
+      expect(cubeValue(cubeOff)).to.equal(-41 * 41 * 41)
+    })
+
+    it('solves example', () => {
+      expect(executeFullRebootSteps(example)).to.equal(39769202357779)
+    })
+
+    it('solve it', () => {
+      expect(executeFullRebootSteps(input)).to.equal(1160303042684776)
     })
   })
 })
